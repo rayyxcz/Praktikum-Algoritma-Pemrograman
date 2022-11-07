8e7d57f8f042550889b2642c8aadaecccad0a9a7
rayyxcz <117646817+rayyxcz@users.noreply.github.com>
2022-11-07 18:56:09 +0700

    Add files via upload

---
+++ Rayhan alim fahrezy - 2270231078 - PAP/pertemuan 1/latihan 4.py
@@ -0,0 +1,25 @@
+#tipe data Boolean
+print(True)
+#tipe data string
+print("Ayo belajar phyton")
+print("belajar phyton sangat mudah")
+#tipe data integer
+print(20)
+#tipe data float
+print(3.14)
+#tipe data hexadecimal
+print("9a")
+#tipe data complex
+print(5j)
+#tipe data list
+print([1,2,3,4,5])
+print(["satu","dua","tiga"])
+#tipe data tuple
+print((1,2,3,4,5))
+print(("satu","dua","tiga"))
+#tipe data dictionary
+print({"nama":"Budi","umur":20})
+#tipe data Dictionary dimasukkan ke dalam variabel biodata
+biodata={"nama":"Andi","umur":21}#proses inisialisasi variabel biodata
+print(biodata)#proses pencetakan variabel biodata yang berisi tipe data dictionary
+print(type(biodata))#fungsi untuk mengecek jenis tipe data. akan tampil <class"dict"> yang berarti dict adalah tipe data dictionary
---
+++ Rayhan alim fahrezy - 2270231078 - PAP/pertemuan 1/latihan 6.py
@@ -0,0 +1,11 @@
+#input user 
+#data yang dimasukkan pasti string
+data=input("masukkan data:")
+print("data=",data,",type=",type(data))
+#jika kita ingin mengambil int,maka
+angka=float(input("masukan data:"))
+angka=int(input("masukan angka:"))
+print("data=",angka,",type=",type(angka))
+#bagaimana dengan boolean
+biner=bool(int(input("masukkan nilai boolean:")))
+print("data=",biner,",type=",type(biner))
