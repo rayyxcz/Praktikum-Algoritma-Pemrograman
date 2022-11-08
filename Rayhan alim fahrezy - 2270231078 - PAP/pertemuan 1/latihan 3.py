2a8830d619c8e0e8d595a373ad2b60517275eb15
rayyxcz <117646817+rayyxcz@users.noreply.github.com>
2022-11-08 07:49:27 +0700

    Add files via upload

---
+++ Rayhan alim fahrezy - 2270231078 - PAP/pertemuan 1/latihan 3.py
@@ -0,0 +1,20 @@
+#Variabel adalah tempat menyimpan data
+#menaru/assignment nilai
+a=10
+x=5
+panjang=1000
+#pemanggilan pertama 
+print("Nilai a=",a)
+print("Nilai x=",x)
+print("Nilai panjang=",panjang)
+#penamaan
+nilai_y=15#dengan menggunakan underscore
+juta10=10000000# ini boleh
+nilaiZ=17.5# ini boleh
+#pemanggilan kedua
+print("nilai a=",a)
+a=7
+print("nilai a=",a)
+#assignment indirect
+b=a
+print("nilai b=",b)
